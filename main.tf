645411c04392f321df519af40952ef9b9c5ac920
user <macszop@gmail.com>
2025-01-24 03:42:06 -0600

    comment out resources

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
